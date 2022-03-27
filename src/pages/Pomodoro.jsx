48787a681e8a3d61bd4d78f1397d5db88a0ec675
luketheduke112 <lukedavidfisher03@gmail.com>
2022-03-26 22:27:04 -0400

    Added link and logic for dynamic data

--- src/pages/Pomodoro.jsx
+++ src/pages/Pomodoro.jsx
@@ -6,16 +6,82 @@ import { faCirclePlay } from '@fortawesome/free-solid-svg-icons'
 import { faPause } from '@fortawesome/free-solid-svg-icons'
 import { faArrowRotateLeft } from "@fortawesome/free-solid-svg-icons"
 import logo from '../assets/logo.svg'
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Line, LineChart} from 'recharts';
+
+
+let sessionCount = 0;
+let numPauses = 0;
+
+let bData = [
+    {
+    name: 'Session 1',
+    breaks: 5,
+    },
+    {
+    name: 'Session 2',
+    breaks: 3,
+    },
+    {
+    name: 'Session 3',
+    breaks: 1,
+    },
+    {
+    name: 'Session 4',
+    breaks: 0,
+    },
+    {
+    name: 'Session 5',
+    breaks: 7,
+    },
+    {
+    name: 'Session 6',
+    breaks: 3,
+    },
+];
+
+let lData = [
+    {
+        name: '3-20-22',
+        sessions: 5,
+      },
+      {
+        name: '3-21-22',
+        sessions: 10,
+      },
+      {
+        name: '3-22-22',
+        sessions: 3,
+      },
+      {
+        name: '3-23-22',
+        sessions: 2,
+      },
+      {
+        name: '3-24-22',
+        sessions: 0,
+      },
+      {
+        name: '3-25-22',
+        sessions: 1,
+      },
+      {
+        name: '3-26-22',
+        sessions: 0,
+      },
+]
+
+
 
 const Home = () => {
+    
 
     const [start, setStart] = useState(false)
-    const [duration, setDuration] = useState(1500)
+    const [duration, setDuration] = useState(5)
     const [key, setKey] = useState(0);
     const [isBreak, setBreak] = useState(false)
     const [isLongBreak, setLongBreak] = useState(false)
     const [tabStyle, setTabStyle] = useState({backgroundColor: "#FF5733", width:'100px', height: '50px', position:'absolute', top: 6, left: 8, borderRadius: '25px'})
+    let chartWidth = "90%";
 
     const PlayButton = ({ running }) => {
         return (
@@ -29,39 +95,14 @@ const Home = () => {
         )
     }
 
-    const BChart = () => {
-        
-        const data = [
-            {
-            name: 'Session 1',
-            breaks: 5,
-            },
-            {
-            name: 'Session 2',
-            breaks: 3,
-            },
-            {
-            name: 'Session 3',
-            breaks: 1,
-            },
-            {
-            name: 'Session 4',
-            breaks: 0,
-            },
-            {
-            name: 'Session 5',
-            breaks: 7,
-            },
-            {
-            name: 'Session 6',
-            breaks: 3,
-            },
-        ];
+    const BChart = ({data}) => {
 
         return (
-            <div className="bchart-container">
-                <h1>Number of Breaks Per Session</h1>
-                <ResponsiveContainer width="100%" aspect={3}>
+            <div className="chart-container">
+                <h1>Number of Pauses Per Session</h1>
+                <h4>Pauses Today: {numPauses}</h4>
+                <p>Breaks are important, but don't get distracted! Try to make it all the way through the sessions you start.</p>
+                <ResponsiveContainer width={chartWidth} aspect={3} className="chart">
                     <BarChart
                     width={500}
                     height={300}
@@ -85,6 +126,38 @@ const Home = () => {
   
     }
 
+    const LChart = ({data}) => {
+    
+        return (
+            <div className="chart-container">
+                <h1>Number of Sessions Per Day</h1>
+                <h4>Sessions Today: {sessionCount}</h4>
+                <p>Pro tip: Try to push yourself and go one more session than the previous day!</p>
+                
+                <ResponsiveContainer width={chartWidth} aspect={3} className="chart">
+                    <LineChart
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                    >
+                    <CartesianGrid strokeDasharray="4 4" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Line type="monotone" dataKey="sessions" stroke="#FF5733" />
+                    </LineChart>
+                </ResponsiveContainer>
+            </div>
+        )
+
+    }
+
     let numPas = 0;
     const renderTime = ({remainingTime}) => {
         let minutes,seconds
@@ -98,7 +171,16 @@ const Home = () => {
     }
 
     const processBreak = () => {
-        setDuration(300)
+        sessionCount++
+        console.log(sessionCount)
+        bData.push(
+            {
+                name: "Session " + sessionCount,
+                breaks: 0
+            }
+        )
+        lData[lData.length-1].sessions++;
+        setDuration(3)
         setKey(key+1) // reset the timer
         setBreak(true) // we are no starting a break
         setStart(false) // stop the timer 
@@ -111,25 +193,37 @@ const Home = () => {
     const pomo = () => {
         setStart(false)
         setKey(key+1)
-        setDuration(1500)
+        setDuration(5)
         setTabStyle({backgroundColor: "#FF5733", width:'100px', height: '50px', position:'absolute', top: 6, left: 8, borderRadius: '25px'})
         setBreak(false)
     }
 
     const longBreak = () => {
         setStart(false)
+        sessionCount++
+        bData.push(
+            {
+                name: "Session " + sessionCount,
+                breaks: 0
+            }
+        )
+        lData[lData.length-1].sessions++;
+        console.log(sessionCount)
         setTabStyle({backgroundColor: "#4CC0FF", width:'110px', height: '50px', position:'absolute', top: 6, left: 222, borderRadius: '25px'})
         setKey(key+1)
-        setDuration(900)
+        setDuration(10)
         setBreak(true)
     }
 
     return (
         <div>
+            <Link className="link" to="/">
             <nav>
                 <h1>Pomo.</h1>
                 <img className='logo' src={logo} alt="" />
             </nav>
+
+            </Link>
             <div className='pomoContainer'>
                 <div className='modeBar'>
                     <div className='indicator' style={tabStyle}></div>
@@ -161,6 +255,10 @@ const Home = () => {
                             setStart(true)
                         } else {
                             setStart(false)
+                            numPauses++;
+                            bData[bData.length-1].breaks++;
+                            console.log(bData[bData.length-1].breaks)
+
                         }
                     }}><PlayButton className='startBtn' running={start}/>
                     </div>
@@ -170,7 +268,9 @@ const Home = () => {
                     }}><FontAwesomeIcon class="icons resetBtn" icon={faArrowRotateLeft} /></div>
                 </div>
             </div>
-            <BChart />
+            <div className="spacer"></div>
+            <LChart data={lData}/>
+            <BChart data={bData}/>
         </div>
         
     ) 
